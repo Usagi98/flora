3f58fe16c053b27c9caf458993b61a986d4cf293
Usagi98 <73405913+Usagi98@users.noreply.github.com>
2021-02-07 16:33:38 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,10 @@
-$(document).ready(() => {
-      let url = location.href.replace(/\/$/, "");
-     
-      if (location.hash) {
-        const hash = url.split("#");
-        $('#myTab a[href="#'+hash[1]+'"]').tab("show");
-        url = location.href.replace(/\/#/, "#");
-        history.replaceState(null, null, url);
-        setTimeout(() => {
-          $(window).scrollTop(0);
-        }, 400);
-      } 
-       
-      $('a[data-toggle="tab"]').on("click", function() {
-        let newUrl;
-        const hash = $(this).attr("href");
-        if(hash == "#home") {
-          newUrl = url.split("#")[0];
-        } else {
-          newUrl = url.split("#")[0] + hash;
-        }
-        newUrl += "/";
-        history.replaceState(null, null, newUrl);
-      });
-    });
+$(function(){
+     var hash = window.location.hash;
+     hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+     $('.nav-tabs a').click(function (e) { 
+           $(this).tab('show');     
+           var scrollmem = $('body').scrollTop();     
+           window.location.hash = this.hash;     
+           $('html,body').scrollTop(scrollmem); 
+     });
+});
