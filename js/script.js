4cccf35c4f090760439a5062528ebd4fbde53843
Usagi98 <73405913+Usagi98@users.noreply.github.com>
2021-02-07 15:46:31 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-var url = window.location.href;     
-if (url.indexOf("#") > 0){     
-    var activeTab = url.substring(url.indexOf("#") + 1);         
-    $('.nav[role="tablist"] a[href="#'+activeTab+'"]').tab('show');     
-}
+$(function(){
+     var hash = window.location.hash;
+     hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+     $('.nav-tabs a').click(function (e) { 
+           $(this).tab('show');     
+           var scrollmem = $('body').scrollTop();     
+           window.location.hash = this.hash;     
+           $('html,body').scrollTop(scrollmem); 
+     });
+});
